8fb94ed2ab99065b73aa25c33f91aced2cbeff25
Your Name <nirajkumar09160@gmail.com>
2026-09-21 22:21:15 +0530

    for admin they can create or update the file and folder

--- storage/admin.py
+++ storage/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Folder, File
+
+
+@admin.register(Folder)
+class FolderAdmin(admin.ModelAdmin):
+      list_display = (
+            "name",
+            "owner",
+            "parent",
+            "created_at",
+            "updated_at",
+      )
+
+      list_filter = (
+            "created_at",
+            "updated_at",
+      )
+
+      search_fields = (
+            "name",
+            "owner__username",
+      )
+
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+      list_display = (
+            "original_name",
+            "owner",
+            "folder",
+            "file_type",
+            "size",
+            "uploaded_at",
+      )
+
+      list_filter = (
+            "file_type",
+            "uploaded_at",
+      )
+
+      search_fields = (
+            "original_name",
+            "owner__username",
+      )
